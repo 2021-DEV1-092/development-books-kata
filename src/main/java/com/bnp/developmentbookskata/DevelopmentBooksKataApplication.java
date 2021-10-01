e2a0f2a7a2d8992fea6cba2e0953ed277e8ee8da
maarten <maarten.callens@ixor.be>
2021-10-01 12:34:32 +0200

    Initial spring boot setup

---
+++ src/main/java/com/bnp/developmentbookskata/DevelopmentBooksKataApplication.java
@@ -0,0 +1,13 @@
+package com.bnp.developmentbookskata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentBooksKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentBooksKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnp/developmentbookskata/DevelopmentBooksKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnp.developmentbookskata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentBooksKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
