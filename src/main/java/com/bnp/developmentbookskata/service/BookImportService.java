c055b30fc49a45c2c1fa62a14765074356d5ff15
maarten <maarten.callens@ixor.be>
2021-10-01 14:30:09 +0200

    Added hardcoded list of books to be returned from BookImportService

--- src/main/java/com/bnp/developmentbookskata/service/BookImportService.java
+++ src/main/java/com/bnp/developmentbookskata/service/BookImportService.java
@@ -9,6 +9,25 @@ import java.util.List;
 public class BookImportService {
 
     public List<Book> importBooks() {
-        return null;
+        return List.of(Book.builder()
+                        .title("Clean Code")
+                        .author("Robert Martin")
+                        .year(2008).build(),
+                Book.builder()
+                        .title("The Clean Coder")
+                        .author("Robert Martin")
+                        .year(2011).build(),
+                Book.builder()
+                        .title("Clean Architecture")
+                        .author("Robert Martin")
+                        .year(2017).build(),
+                Book.builder()
+                        .title("Test Driven Development by Example")
+                        .author("Kent Beck")
+                        .year(2003).build(),
+                Book.builder()
+                        .title("Working Effectively With Legacy Code")
+                        .author("Michael C. Feathers")
+                        .year(2001).build());
     }
 }
--- src/test/java/com/bnp/developmentbookskata/service/BookImportServiceTest.java
+++ src/test/java/com/bnp/developmentbookskata/service/BookImportServiceTest.java
@@ -10,11 +10,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BookImportServiceTest {
 
     @Test
-    public void verifyBooksImport(){
+    public void verifyImportBooksReturnsExpectedBooks() {
         BookImportService bookImportService = new BookImportService();
         List<Book> importedBooks = bookImportService.importBooks();
-        Book expectedBook1 = Book.builder().title("Clean Code").title("Robert Martin").year(2008).build();
-        Book expectedBook2 = Book.builder().title("The Clean Coder").title("Robert Martin").year(2001).build();
-        assertThat(importedBooks).containsExactly(expectedBook1, expectedBook2);
+        Book expectedBook1 = Book.builder()
+                .title("Clean Code")
+                .author("Robert Martin")
+                .year(2008).build();
+        Book expectedBook2 = Book.builder()
+                .title("The Clean Coder")
+                .author("Robert Martin")
+                .year(2011).build();
+        Book expectedBook3 = Book.builder()
+                .title("Clean Architecture")
+                .author("Robert Martin")
+                .year(2017).build();
+        Book expectedBook4 = Book.builder()
+                .title("Test Driven Development by Example")
+                .author("Kent Beck")
+                .year(2003).build();
+        Book expectedBook5 = Book.builder()
+                .title("Working Effectively With Legacy Code")
+                .author("Michael C. Feathers")
+                .year(2001).build();
+        assertThat(importedBooks).containsExactly(expectedBook1, expectedBook2, expectedBook3, expectedBook4, expectedBook5);
+        assertThat(importedBooks).hasSize(5);
     }
+
 }
