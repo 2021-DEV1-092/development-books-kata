081f2eeb198aec1d4a30741e114f77c11abfcb5a
maarten <maarten.callens@ixor.be>
2021-10-01 17:54:37 +0200

    Basic implementation of algorithm to calculate set of two distinct books.

--- src/main/java/com/bnp/developmentbookskata/service/PriceCalculationService.java
+++ src/main/java/com/bnp/developmentbookskata/service/PriceCalculationService.java
@@ -3,14 +3,55 @@ package com.bnp.developmentbookskata.service;
 import com.bnp.developmentbookskata.model.Book;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class PriceCalculationService {
 
-    private static final Double BOOK_PRICE = 50d;
+    private static final double BOOK_PRICE = 50d;
+    private static final double TWO_BOOK_DISCOUNT_PERCENTAGE = 5;
 
     public Double calculatePrice(List<Book> bookList) {
-        return bookList.size() * BOOK_PRICE;
+        List<Set<Book>> bookListSets = new ArrayList<>();
+        Set<Book> initialSet = new HashSet<>();
+
+        bookListSets.add(initialSet);
+
+        groupBooksInUniqueSets(bookList, bookListSets);
+
+        double totalPrice = 0d;
+        for (Set<Book> set : bookListSets) {
+            if (set.size() == 1) {
+                totalPrice += set.size() * BOOK_PRICE;
+            }
+            if (set.size() == 2) {
+                double basePrice = set.size() * BOOK_PRICE;
+                double discountedPriceForSet = basePrice - ((basePrice * TWO_BOOK_DISCOUNT_PERCENTAGE) / 100.0);
+                totalPrice += discountedPriceForSet;
+            }
+        }
+
+        return totalPrice;
     }
+
+    private void groupBooksInUniqueSets(List<Book> bookList, List<Set<Book>> bookListSets) {
+        for (Book book : bookList) {
+            boolean bookAddedToExistingSet = false;
+            for (Set<Book> set : bookListSets) {
+                if (!set.contains(book)) {
+                    set.add(book);
+                    bookAddedToExistingSet = true;
+                    break;
+                }
+            }
+            if (!bookAddedToExistingSet) {
+                Set<Book> newSet = new HashSet<>();
+                newSet.add(book);
+                bookListSets.add(newSet);
+            }
+        }
+    }
+
 }
--- src/test/java/com/bnp/developmentbookskata/service/PriceCalculationServiceTest.java
+++ src/test/java/com/bnp/developmentbookskata/service/PriceCalculationServiceTest.java
@@ -1,7 +1,8 @@
 package com.bnp.developmentbookskata.service;
 
 import com.bnp.developmentbookskata.model.Book;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.List;
 
@@ -9,13 +10,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PriceCalculationServiceTest {
 
+    private Book book_1;
+    private Book book_2;
+
+    @Before
+    public void setUp(){
+        book_1 = Book.builder()
+                .title("Clean Code")
+                .author("Robert Martin")
+                .year(2008).build();
+
+        book_2 =  Book.builder()
+                .title("The Clean Coder")
+                .author("Robert Martin")
+                .year(2011).build();
+    }
+
     @Test
     public void verifyOneBookReturnsCorrectPrice() {
         PriceCalculationService service = new PriceCalculationService();
-        List<Book> bookList = List.of(Book.builder()
-                        .title("Clean Code")
-                        .author("Robert Martin")
-                        .year(2008).build());
+        List<Book> bookList = List.of(book_1);
         Double calculatedPrice = service.calculatePrice(bookList);
         assertThat(calculatedPrice).isEqualTo(50);
     }
@@ -23,16 +37,25 @@ public class PriceCalculationServiceTest {
     @Test
     public void verifyTwoSameBooksReturnsCorrectPrice() {
         PriceCalculationService service = new PriceCalculationService();
-        List<Book> bookList = List.of(Book.builder()
-                .title("Clean Code")
-                .author("Robert Martin")
-                .year(2008).build(),
-                Book.builder()
-                .title("Clean Code")
-                .author("Robert Martin")
-                .year(2008).build());
+        List<Book> bookList = List.of(book_1, book_1);
         Double calculatedPrice = service.calculatePrice(bookList);
         assertThat(calculatedPrice).isEqualTo(100);
+    }
+
+    @Test
+    public void verifyTwoDiffBooksReturnsCorrectPrice() {
+        PriceCalculationService service = new PriceCalculationService();
+        List<Book> bookList = List.of(book_1, book_2);
+        Double calculatedPrice = service.calculatePrice(bookList);
+        assertThat(calculatedPrice).isEqualTo(95);
+    }
+
+    @Test
+    public void verifyFiveSameBooksThreeDiffBooksReturnsCorrectPrice() {
+        PriceCalculationService service = new PriceCalculationService();
+        List<Book> bookList = List.of(book_1, book_1, book_1, book_1, book_1, book_2, book_2, book_2);
+        Double calculatedPrice = service.calculatePrice(bookList);
+        assertThat(calculatedPrice).isEqualTo(385);
     }
 }
 
